35b7e7cc3211d75062803f52ff90a030f88a5533
SuperArilo <superarilowh@gmail.com>
2025-01-08 15:20:48 +0800

    fix sql

--- src/main/java/ari/superarilo/enumType/sql/CreateTableSql.java
+++ src/main/java/ari/superarilo/enumType/sql/CreateTableSql.java
@@ -5,15 +5,15 @@ public enum CreateTableSql {
             "player_home",
             """
                     CREATE TABLE IF NOT EXISTS player_home (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        home_id TEXT NOT NULL,
-                        home_name TEXT NOT NULL,
-                        player_uuid TEXT NOT NULL,
+                        id INT AUTO_INCREMENT PRIMARY KEY,
+                        home_id VARCHAR(128) NOT NULL,
+                        home_name VARCHAR(128) NOT NULL,
+                        player_uuid VARCHAR(128) NOT NULL,
                         x DECIMAL(10, 3) NOT NULL,
                         y DECIMAL(10, 3) NOT NULL,
                         z DECIMAL(10, 3) NOT NULL,
-                        world TEXT NOT NULL,
-                        show_material TEXT NOT NULL
+                        world VARCHAR(128) NOT NULL,
+                        show_material VARCHAR(128) NOT NULL
                     )""");
     private final String tableName;
     private final String sql;

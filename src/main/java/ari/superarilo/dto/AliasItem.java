a74a9d56d9c1dde36e9853ffb8f6c8d53e9854f4
SuperArilo <728152568@qq.com>
2025-03-21 10:58:27 +0800

    fix bug

--- src/main/java/ari/superarilo/dto/AliasItem.java
+++ src/main/java/ari/superarilo/dto/AliasItem.java
@@ -1,9 +1,12 @@
 package ari.superarilo.dto;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 @Data
 public class AliasItem {
+    @SerializedName("enable")
     private boolean enable;
+    @SerializedName("tab-complete")
     private boolean tabComplete;
 }

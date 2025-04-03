d4081d062a15dd046122907c0b561312a4fadf97
SuperArilo <728152568@qq.com>
2025-04-03 10:34:12 +0800

    update

--- src/main/java/ari/superarilo/command/function/CommandBack.java
+++ src/main/java/ari/superarilo/command/function/CommandBack.java
@@ -4,7 +4,6 @@ import ari.superarilo.Ari;
 import ari.superarilo.enumType.FilePath;
 import ari.superarilo.function.TeleportCheck;
 import ari.superarilo.function.TeleportThread;
-import ari.superarilo.function.impl.TeleportThreadImpl;
 import ari.superarilo.tool.TextTool;
 import org.bukkit.Location;
 import org.bukkit.command.CommandSender;
@@ -20,13 +19,13 @@ public class CommandBack {
 
     public void startDo() {
         Player player = (Player) this.sender;
-        Location beforeLocation = TeleportThreadImpl.lastLocation.get(player.getUniqueId());
+        Location beforeLocation = TeleportThread.lastLocation.get(player.getUniqueId());
         if(beforeLocation == null) {
             player.sendMessage(TextTool.setHEXColorText("teleport.none-location", FilePath.Lang));
             return;
         }
        if(TeleportCheck.create().preCheckStatus(player, beforeLocation)) {
-           TeleportThread.playerToLocation(player, beforeLocation) .teleport(Ari.instance.configManager.getValue("main.teleport.delay", FilePath.TPA, Integer.class));
+           ari.superarilo.function.TeleportThread.playerToLocation(player, beforeLocation) .teleport(Ari.instance.configManager.getValue("main.teleport.delay", FilePath.TPA, Integer.class));
        }
     }
 
--- src/main/java/ari/superarilo/listener/teleport/RecordLastLocationListener.java
+++ src/main/java/ari/superarilo/listener/teleport/RecordLastLocationListener.java
@@ -1,7 +1,7 @@
 package ari.superarilo.listener.teleport;
 
 import ari.superarilo.dto.event.CustomPlayerRespawnEvent;
-import ari.superarilo.function.impl.TeleportThreadImpl;
+import ari.superarilo.function.TeleportThread;
 import ari.superarilo.tool.TextTool;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -14,22 +14,22 @@ import org.bukkit.event.player.PlayerTeleportEvent;
 public class RecordLastLocationListener implements Listener {
     @EventHandler
     public void lastLocation(PlayerTeleportEvent event) {
-        TeleportThreadImpl.lastLocation.put(event.getPlayer().getUniqueId(), event.getFrom());
+        TeleportThread.lastLocation.put(event.getPlayer().getUniqueId(), event.getFrom());
     }
     @EventHandler
     public void lastDeathLocation(PlayerDeathEvent event) {
-        TeleportThreadImpl.lastLocation.put(event.getPlayer().getUniqueId(), event.getPlayer().getLocation());
+        TeleportThread.lastLocation.put(event.getPlayer().getUniqueId(), event.getPlayer().getLocation());
     }
     @EventHandler
     public void onRespawn(CustomPlayerRespawnEvent event) {
         Player player = event.getPlayer();
-        Location deathLocation = TeleportThreadImpl.lastLocation.get(player.getUniqueId());
+        Location deathLocation = TeleportThread.lastLocation.get(player.getUniqueId());
         if (deathLocation == null) return;
         String s = TextTool.XYZText(deathLocation.getX(), deathLocation.getY(), deathLocation.getZ());
         event.getPlayer().sendMessage(TextTool.setHEXColorText("&b上次死亡的地点: " + s + " &7输入/back返回"));
     }
     @EventHandler
     public void cleanPlayerLastLocation(PlayerQuitEvent event) {
-        TeleportThreadImpl.lastLocation.remove(event.getPlayer().getUniqueId());
+        TeleportThread.lastLocation.remove(event.getPlayer().getUniqueId());
     }
 }

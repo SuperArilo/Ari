a959d1bcde1c91d9bc7141b98b824a8cb91117d7
SuperArilo <superarilowh@gmail.com>
2025-06-11 18:59:55 +0800

    添加第一次进入服务器设置是否传送到新的出生点

--- instance/src/main/java/com/tty/listener/player/OnPlayerListener.java
+++ instance/src/main/java/com/tty/listener/player/OnPlayerListener.java
@@ -3,9 +3,12 @@ package com.tty.listener.player;
 import com.tty.Ari;
 import com.tty.enumType.FilePath;
 import com.tty.function.PlayerManager;
+import com.tty.lib.EntityTeleport;
+import com.tty.lib.Lib;
 import com.tty.tool.Log;
 import com.tty.tool.TextTool;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -24,17 +27,27 @@ public class OnPlayerListener implements Listener {
     private static final Map<UUID, Long> playerLoginTimes = new HashMap<>();
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        boolean first = Ari.instance.getConfig().getBoolean("server.message.on-first-login", false);
+        boolean login = Ari.instance.getConfig().getBoolean("server.message.on-login", false);
         Player player = event.getPlayer();
         PlayerManager build = PlayerManager.build(player);
-        event.joinMessage(null);
+        if (first || login) {
+            event.joinMessage(null);
+        }
         build.asyncGetInstance(player.getUniqueId().toString()).thenAccept(i -> {
             if (i == null || !player.hasPlayedBefore()) {
-                if(Ari.instance.getConfig().getBoolean("server.message.on-first-login")) {
+                if (Ari.instance.getConfig().getBoolean("server.spawn.first-join", false)) {
+                    Lib.Scheduler.runAtEntity(Ari.instance, player, a -> {
+                        Location spawnLocation = player.getWorld().getSpawnLocation();
+                        EntityTeleport.teleport(player, spawnLocation);
+                    }, () -> {});
+                }
+                if(first) {
                     Bukkit.broadcast(TextTool.setHEXColorText("server.message.on-first-login", FilePath.Lang, player));
                 }
                 build.createInstance(player.getUniqueId().toString());
             } else {
-                if(Ari.instance.getConfig().getBoolean("server.message.on-login")) {
+                if(login) {
                     Bukkit.broadcast(TextTool.setHEXColorText("server.message.on-login", FilePath.Lang, player));
                 }
             }

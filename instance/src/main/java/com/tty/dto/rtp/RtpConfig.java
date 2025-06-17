efa6db96b508a41be857c9aca884f6306a142377
SuperArilo <superarilowh@gmail.com>
2025-06-17 22:59:08 +0800

    fix

--- instance/src/main/java/com/tty/dto/rtp/RtpConfig.java
+++ instance/src/main/java/com/tty/dto/rtp/RtpConfig.java
@@ -7,6 +7,8 @@ import lombok.Data;
 public class RtpConfig {
     @Expose
     private boolean enable = true;
+    @Expose
     private double min = 300.0;
+    @Expose
     private double max = 1500.0;
 }

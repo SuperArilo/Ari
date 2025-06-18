df4755c8a86d1d72ec4c5c6b0a432893b9823c2b
SuperArilo <728152568@qq.com>
2025-06-18 09:23:31 +0800

    update

--- instance/src/main/java/com/tty/listener/OnPluginReloadListener.java
+++ instance/src/main/java/com/tty/listener/OnPluginReloadListener.java
@@ -2,7 +2,10 @@ package com.tty.listener;
 
 import com.tty.Ari;
 import com.tty.dto.event.CustomPluginReloadEvent;
+import com.tty.enumType.FilePath;
 import com.tty.tool.ConfigObjectUtils;
+import com.tty.tool.TextTool;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
@@ -10,8 +13,16 @@ public class OnPluginReloadListener implements Listener {
 
     @EventHandler
     public void pluginReload(CustomPluginReloadEvent event) {
+        Ari.reloadAllConfig();
+        if (Ari.debug) {
+            Ari.instance.SQLInstance.reconnect();
+        }
         Ari.instance.commandAlias.reloadAllAlias();
         ConfigObjectUtils.setRtpWorldConfig();
+        Object entity = event.getEntity();
+        if (entity instanceof CommandSender commandSender) {
+            commandSender.sendMessage(TextTool.setHEXColorText("function.reload.success", FilePath.Lang));
+        }
     }
 
 }

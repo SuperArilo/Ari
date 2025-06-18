e091a5cb4b3a5693c286192a188776c06b26211e
SuperArilo <728152568@qq.com>
2025-06-18 09:23:22 +0800

    update

--- instance/src/main/java/com/tty/dto/event/CustomPluginReloadEvent.java
+++ instance/src/main/java/com/tty/dto/event/CustomPluginReloadEvent.java
@@ -1,15 +1,22 @@
 package com.tty.dto.event;
 
 import lombok.Getter;
+import org.bukkit.entity.Entity;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
-public class CustomPluginReloadEvent  extends Event implements Cancellable {
+public class CustomPluginReloadEvent<T> extends Event implements Cancellable {
     @Getter
     private final static HandlerList handlerList = new HandlerList();
     private boolean isCancelled = false;
+    @Getter
+    private T entity;
+
+    public CustomPluginReloadEvent(T entity) {
+        this.entity = entity;
+    }
 
     @Override
     public boolean isCancelled() {
@@ -25,4 +32,5 @@ public class CustomPluginReloadEvent  extends Event implements Cancellable {
     public @NotNull HandlerList getHandlers() {
         return handlerList;
     }
+
 }

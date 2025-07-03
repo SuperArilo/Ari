7b39e95a9438c12b4a61e4a949d2f58ed1d5fe0b
SuperArilo <728152568@qq.com>
2025-07-03 11:08:08 +0800

    update

--- lib/src/main/java/com/tty/lib/dto/Page.java
+++ lib/src/main/java/com/tty/lib/dto/Page.java
@@ -2,19 +2,13 @@ package com.tty.lib.dto;
 
 import lombok.Getter;
 
+@Getter
 public class Page {
-    private final int pageNum;
-    private final int pageSize;
-    //控制每页的记录数
-    @Getter
+
     private final int limit;
-    //控制查询从哪一条记录开始
-    @Getter
     private final int offset;
 
     private Page(int pageNum, int pageSize) {
-        this.pageNum = pageNum;
-        this.pageSize = pageSize;
         this.limit = pageSize;
         this.offset = (pageNum - 1) * pageSize;
     }

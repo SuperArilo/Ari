6198b7cd3a0a4bf8944e73ce317d145f45466d8d
SuperArilo <superarilowh@gmail.com>
2025-11-27 20:07:18 +0800

    修复指令无法执行的问题

--- instance/src/main/java/com/tty/commands/setspawn.java
+++ instance/src/main/java/com/tty/commands/setspawn.java
@@ -15,7 +15,7 @@ import java.util.List;
 public class setspawn extends BaseCommand<String> {
 
     public setspawn() {
-        super(false, StringArgumentType.string(), 2);
+        super(false, StringArgumentType.string(), 1);
     }
 
     @Override

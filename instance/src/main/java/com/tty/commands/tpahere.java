9b6d38968164cfd9ce5df78ed5a0212e30ac7aba
SuperArilo <superarilowh@gmail.com>
2025-11-29 21:51:46 +0800

    update

--- instance/src/main/java/com/tty/commands/tpahere.java
+++ instance/src/main/java/com/tty/commands/tpahere.java
@@ -41,7 +41,7 @@ public class tpahere extends TpaBase<PlayerSelectorArgumentResolver> {
                 .addState(new PreEntityToEntityState(
                         owner,
                         player,
-                        Ari.C_INSTANCE.getValue("main.teleport.cooldown", FilePath.TPA_CONFIG, Integer.class, 3),
+                        Ari.C_INSTANCE.getValue("main.teleport.cooldown", FilePath.TPA_CONFIG, Integer.class, 10),
                         this.name()));
     }
 

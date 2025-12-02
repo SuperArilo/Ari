fdad3c119e03e7f074508968077f52824c578d61
SuperArilo <728152568@qq.com>
2025-12-02 14:54:29 +0800

    update

--- src/main/java/com/tty/listener/warp/WarpListListener.java
+++ src/main/java/com/tty/listener/warp/WarpListListener.java
@@ -33,6 +33,7 @@ import java.util.UUID;
 
 public class WarpListListener extends BaseGuiListener {
 
+    private final WarpManager warpManager = new WarpManager(true);
     private final NamespacedKey TYPE_KEY = new NamespacedKey(Ari.instance, "type");
     private final NamespacedKey WARP_ID_KEY = new NamespacedKey(Ari.instance, "warp_id");
 
@@ -58,7 +59,7 @@ public class WarpListListener extends BaseGuiListener {
             case DATA -> {
                 String warpId = currentItem.getItemMeta().getPersistentDataContainer().get(this.WARP_ID_KEY, PersistentDataType.STRING);
                 //从数据库查询最新的
-                new WarpManager(true).getInstance(warpId).thenAccept((instance) -> {
+                this.warpManager.getInstance(warpId).thenAccept((instance) -> {
                     if (instance == null) {
                         Log.error("can't find warpId: %s", warpId);
                         return;

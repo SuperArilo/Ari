037e59fc6cb5c36e3f29931a0535c8ed8d2d7f65
SuperArilo <superarilowh@gmail.com>
2025-12-04 19:26:57 +0800

    修复 op 传送会有传送时间问题

--- src/main/java/com/tty/dto/state/teleport/PlayerToPlayerState.java
+++ src/main/java/com/tty/dto/state/teleport/PlayerToPlayerState.java
@@ -3,6 +3,7 @@ package com.tty.dto.state.teleport;
 import com.tty.lib.enum_type.TeleportType;
 import lombok.Getter;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 public class PlayerToPlayerState extends EntityToEntityState {
 
@@ -10,7 +11,7 @@ public class PlayerToPlayerState extends EntityToEntityState {
     private final String command;
 
     public PlayerToPlayerState(Entity owner, Entity target, int max_count, String command) {
-        super(owner, TeleportType.TPA, target, max_count);
+        super(owner, TeleportType.TPA, target, (owner instanceof Player p && p.isOp()) ? 0:max_count);
         this.command = command;
     }
 }
